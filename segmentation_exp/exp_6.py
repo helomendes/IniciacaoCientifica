c20ca039e3390dcac8685769dc405fec6e73f5b3
Heloisa <heloisabmendes@gmail.com>
2024-09-26 13:09:59 -0300

    intensity transformations

--- segmentation_exp/exp_6.py
+++ segmentation_exp/exp_6.py
@@ -4,6 +4,7 @@ import os
 from glob import glob
 import cv2 as cv
 import numpy as np
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', help='Configuration YAML file')
@@ -15,21 +16,43 @@ with open(args.config, 'r') as file:
 imgs_dir = os.path.abspath(config.get('images_dir')) + '/'
 dest_dir = os.path.abspath(config.get('destination_dir')) + '/'
 
-color = np.uint8([[[122, 142, 150]]])
+color = np.uint8([[[112, 127, 174]]])
 hsv_color = cv.cvtColor(color, cv.COLOR_BGR2HSV)
 frst = hsv_color[0][0][0]
-hsv_lower = np.array([frst - 10, 100, 100])
-hsv_upper = np.array([frst + 10, 255, 255])
+hsv_lower = np.array([frst - 5, 100, 150])
+hsv_upper = np.array([frst + 5, 255, 255])
+
+hsv = hsv_color[0][0]
+code = f'{hsv[0]}_{hsv[1]}_{hsv[2]}'
+
+color = np.uint8([[[88, 91, 118]]])
+hsv_color = cv.cvtColor(color, cv.COLOR_BGR2HSV)
+frst = hsv_color[0][0][0]
+hsv_lower_2 = np.array([frst - 3, 80, 120])
+hsv_upper_2 = np.array([frst + 3, 255, 255])
 
 imgs = sorted(glob(f'{imgs_dir}/*'))
+asw = input('Save?')
 for image in imgs:
-    img = cv.imread(image)
+    img_name = os.path.split(image)[-1][:-4]
+    img = cv.imread(image) 
+    img = cv.resize(img, (0,0), None, .50, .50)
     img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    mask = cv.inRange(img_hsv, hsv_lower, hsv_upper)
+    mask1 = cv.inRange(img_hsv, hsv_lower, hsv_upper)
+    mask2 = cv.inRange(img_hsv, hsv_lower_2, hsv_upper_2)
+    mask = cv.bitwise_or(mask1, mask2)
     res = cv.bitwise_and(img, img, mask=mask)
     nocolor = cv.bitwise_and(img, img, mask=cv.bitwise_not(mask).astype(np.uint8))
-    #cv.imshow('Original Image', res)
-    cv.imshow('Image', res)
-    
-    cv.waitKey()
 
+    mask_3_channel = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+    numpy_horizontal_1 = np.hstack((img, mask_3_channel))
+    numpy_horizontal_2 = np.hstack((res, nocolor))
+    numpy_vertical = np.vstack((numpy_horizontal_1, numpy_horizontal_2))
+
+    cv.imshow('Np Vertical', numpy_vertical)
+    cv.waitKey()
+    if asw == 'y' or asw == 'Y':
+        today = datetime.now()
+        save_dir = f'/home/hbm22/hmmetria/experiments/{today.strftime("%Y_%m_%d")}/{code}'
+        os.makedirs(save_dir, exist_ok=True)
+        cv.imwrite(f'{save_dir}/{img_name}.jpg', numpy_vertical)
---
+++ segmentation_exp/intensity_transformation.py
@@ -0,0 +1,85 @@
+import argparse
+from yaml import safe_load
+import os
+from glob import glob
+import cv2 as cv
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', help='Configuration YAML file')
+args = parser.parse_args()
+
+with open(args.config, 'r') as file:
+    config = safe_load(file)
+
+imgs_dir = os.path.abspath(config.get('images_dir')) + '/'
+dest_dir = os.path.abspath(config.get('destination_dir')) + '/'
+imgs = sorted(glob(f'{imgs_dir}/*'))
+
+for image in imgs:
+    img_name = os.path.split(image)[-1][:-4]
+    img = cv.imread(image)
+    #img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    ###### LOGARITHMIC TRANSFORMATION #####
+
+    '''
+    c = 1
+    log_transformed = c * np.log(1 + img)
+    c_1 = np.array(log_transformed, dtype = np.uint8)
+
+    c = 25
+    log_transformed = c * np.log(1 + img)
+    c_25 = np.array(log_transformed, dtype = np.uint8)
+
+    c = 50
+    log_transformed = c * np.log(1 + img)
+    c_50 = np.array(log_transformed, dtype = np.uint8)
+
+    c = 75
+    log_transformed = c * np.log(1 + img)
+    c_75 = np.array(log_transformed, dtype = np.uint8)
+
+    c = 150
+    log_transformed = c * np.log(1 + img)
+    c_150 = np.array(log_transformed, dtype = np.uint8)
+
+    c = 255/(np.log(1 + np.int16(np.max(img))))
+    log_transformed = c * np.log(1 + img)
+    c_perfect = np.array(log_transformed, dtype = np.uint8)
+    
+    titles = ['Original', 'C = 1', 'C = 25', 'C = 50', 'C = 75', 'C = 150', 'Perfect C']
+    images = [img, c_1, c_25, c_50, c_75, c_150, c_perfect]
+
+    for i in range(7):
+        plt.subplot(3, 3, i+1)
+        plt.imshow(images[i], plt.cm.gray)
+        plt.title(titles[i])
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+
+    '''
+
+    ##### POWER-LAW (GAMMA) TRANSFORMATION ######
+
+    '''
+    
+    i = 1
+    plt.subplot(3,3,i)
+    plt.imshow(img, plt.cm.gray)
+    plt.title('Original')
+    plt.axis('off')
+
+    for gamma in [0.1, 0.5, 1.2, 2.2]:
+        i += 1
+        gamma_corrected = np.array(255*(img/255) ** gamma, dtype=np.uint8)
+        plt.subplot(3,3,i)
+        plt.imshow(gamma_corrected, plt.cm.gray)
+        plt.title(f'Gamma {gamma}')
+        plt.axis('off')
+    plt.show()
+
+    '''
