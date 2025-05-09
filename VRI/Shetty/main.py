b375342db3ea76fabef35b1d44c7975f79053187
Heloisa <heloisabmendes@gmail.com>
2025-05-09 12:20:56 -0300

    finer control model training

--- VRI/Shetty/main.py
+++ VRI/Shetty/main.py
@@ -5,6 +5,7 @@ import glob
 import random
 import tensorflow as tf
 import math
+import numpy as np
 
 def main():
     parse = argparse.ArgumentParser()
@@ -24,6 +25,9 @@ def main():
     h = 220
     w = 220
 
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    '''
     train_full = tf.keras.utils.image_dataset_from_directory(
             images_dir,
             validation_split = 0.2,
@@ -44,12 +48,55 @@ def main():
             batch_size = batch_size)
 
     num_classes = len(val_ds.class_names)
+    class_names = val_ds.class_names
 
-    normalization_layer = tf.keras.layers.Rescaling(1./255)
-
-    AUTOTUNE = tf.data.AUTOTUNE
     train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    '''
+
+    # finer control
+
+    list_ds = tf.data.Dataset.list_files(f'{images_dir}*/*', shuffle=False)
+    list_ds = list_ds.shuffle(images_count, reshuffle_each_iteration=False)
+    class_names = np.array(sorted([item.split('/')[-1] for item in glob.glob(f'{images_dir}*')]))
+    num_classes = len(class_names)
+
+    # its counting all the images, but in this experiment we will use only 700 images from the dataset
+    # can I just adjust it to 700 instead of 10015?
+    
+    val_size = int(images_count*0.2)
+    train_ds = list_ds.skip(val_size)
+    val_ds = list_ds.take(val_size)
+
+    def get_label(file_path):
+        parts = tf.strings.split(file_path, os.path.sep)
+        one_hot = parts[-2] == class_names
+        return tf.argmax(one_hot)
+
+    def decode_img(img):
+        img = tf.io.decode_jpeg(img, channels=3)
+        return tf.image.resize(img, [h, w])
+
+    def process_path(file_path):
+        label = get_label(file_path)
+        img = tf.io.read_file(file_path)
+        img = decode_img(img)
+        return img, label
+
+    train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+    val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+    def configure_for_performance(ds):
+        ds = ds.cache()
+        ds = ds.shuffle(buffer_size=1000)
+        ds = ds.batch(batch_size)
+        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        return ds
+
+    train_ds = configure_for_performance(train_ds)
+    val_ds = configure_for_performance(val_ds)
+
+    normalization_layer = tf.keras.layers.Rescaling(1./255)
 
     model = tf.keras.Sequential([
         tf.keras.layers.Rescaling(1./255),
@@ -75,13 +122,6 @@ def main():
             validation_data = val_ds,
             epochs=3
             )
-
-    # finer control
-
-    list_ds = tf.data.Dataset.list_files(f'{images_dir}*/*', shuffle=False)
-    list_ds = list_ds.shuffle(image_count, reshuffle_each_interation=False)
-
-
 
 if __name__ == "__main__":
     main()
