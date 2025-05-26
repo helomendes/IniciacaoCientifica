eb43a0d54c1ac4594306d57699fcefd852551468
Heloisa <heloisabmendes@gmail.com>
2025-05-26 12:02:24 -0300

    code modularization

--- VRI/Shetty/main.py
+++ VRI/Shetty/main.py
@@ -7,38 +7,8 @@ import tensorflow as tf
 import math
 import numpy as np
 
-def main():
-    parse = argparse.ArgumentParser()
-    parse.add_argument('--config', help='YAML configuration file', required=True)
-    args = parse.parse_args()
-    with open(args.config, 'r') as file:
-        config = yaml.safe_load(file)
-
-    images_dir = os.path.abspath(config.get('images_dir')) + '/'
-    train_size = config.get('train_size')
-
-    image_paths = []
-    class_names = sorted([name for name in glob.glob(f'{images_dir}*/')])
-    for name in class_names:
-        ds = glob.glob(f'{name}/*')
-        selected = random.sample(ds, train_size)
-        image_paths.extend(selected)
-    random.shuffle(image_paths)
-    images_count = len(image_paths)
-
-    num_classes = len(class_names)
-
-    #images_count = len(list(glob.glob(f'{images_dir}*/*')))
-
-    # 220 * 220
-    # for the CNN model images are scaled to 96x96 with a depth of 3
-    batch_size = 16
-    h = 220
-    w = 220
-
-    AUTOTUNE = tf.data.AUTOTUNE
-
-    '''
+# NORMAL TRAINING
+'''
     train_full = tf.keras.utils.image_dataset_from_directory(
             images_dir,
             validation_split = 0.2,
@@ -63,46 +33,88 @@ def main():
 
     train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-    '''
+'''
 
-    # finer control
+def getDataset(images_dir, train_size):
+    image_paths = []
+    class_names = sorted([name for name in glob.glob(f'{images_dir}*/')])
+    for name in class_names:
+        ds = glob.glob(f'{name}/*')
+        selected = random.sample(ds, train_size)
+        image_paths.extend(selected)
+    random.shuffle(image_paths)
+    return image_paths, len(image_paths), class_names
 
-    list_ds = tf.data.Dataset.list_files(image_paths, shuffle=False)
-    list_ds = list_ds.shuffle(images_count, reshuffle_each_iteration=False)
+class Model:
+    def __init__(self, image_paths, image_count, class_names):
+        self.batch_size = 16
+        # 220 * 220
+        # for the CNN model images are scaled to 96x96 with a depth of 3
+        self.h = 220
+        self.w = 220
+        self.AUTOTUNE = tf.data.AUTOTUNE
+        self.image_paths = image_paths
+        self.images_count = image_count
+        self.class_names = class_names
 
-    val_size = int(images_count*0.2)
-    train_ds = list_ds.skip(val_size)
-    val_ds = list_ds.take(val_size)
+    def dataset_prep(self):
+        # finer control
+        list_ds = tf.data.Dataset.list_files(self.image_paths, shuffle=False)
+        list_ds = list_ds.shuffle(self.images_count, reshuffle_each_iteration=False)
 
-    def get_label(file_path):
+        val_size = int(self.images_count*0.2)
+        self.train_ds = list_ds.skip(val_size)
+        self.val_ds = list_ds.take(val_size)
+
+    def get_label(self, file_path):
         parts = tf.strings.split(file_path, os.path.sep)
-        one_hot = parts[-2] == class_names
+        one_hot = parts[-2] == self.class_names
         return tf.argmax(one_hot)
 
-    def decode_img(img):
+    def decode_img(self, img):
         img = tf.io.decode_jpeg(img, channels=3)
-        return tf.image.resize(img, [h, w])
+        return tf.image.resize(img, [self.h, self.w])
 
-    def process_path(file_path):
-        label = get_label(file_path)
+    def process_path(self, file_path):
+        label = self.get_label(file_path)
         img = tf.io.read_file(file_path)
-        img = decode_img(img)
+        img = self.decode_img(img)
         return img, label
 
-    train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-    val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-
-    def configure_for_performance(ds):
+    def configure_for_performance(self, ds):
         ds = ds.cache()
         ds = ds.shuffle(buffer_size=1000)
-        ds = ds.batch(batch_size)
-        ds = ds.prefetch(buffer_size=AUTOTUNE)
+        ds = ds.batch(self.batch_size)
+        ds = ds.prefetch(buffer_size=self.AUTOTUNE)
         return ds
 
-    train_ds = configure_for_performance(train_ds)
-    val_ds = configure_for_performance(val_ds)
+    def train_val(self):
+        self.train_ds = self.train_ds.map(self.process_path, num_parallel_calls=self.AUTOTUNE)
+        self.val_ds = self.val_ds.map(self.process_path, num_parallel_calls=self.AUTOTUNE)
 
-    normalization_layer = tf.keras.layers.Rescaling(1./255)
+        self.train_ds = self.configure_for_performance(self.train_ds)
+        self.val_ds = self.configure_for_performance(self.val_ds)
+
+def main():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--config', help='YAML configuration file', required=True)
+    args = parse.parse_args()
+    with open(args.config, 'r') as file:
+        config = yaml.safe_load(file)
+
+    images_dir = os.path.abspath(config.get('images_dir')) + '/'
+    train_size = config.get('train_size')
+
+    image_paths, images_count, class_names = getDataset(images_dir, train_size)
+    num_classes = len(class_names)
+
+    #images_count = len(list(glob.glob(f'{images_dir}*/*')))
+
+    modelo = Model(image_paths, images_count, class_names)
+    modelo.dataset_prep()
+    modelo.train_val()
+
+    modelo.normalization_layer = tf.keras.layers.Rescaling(1./255)
 
     model = tf.keras.Sequential([
         tf.keras.layers.Rescaling(1./255),
@@ -124,13 +136,12 @@ def main():
             )
 
     model.fit(
-            train_ds,   # x: input data
+            modelo.train_ds,   # x: input data
                         # if x is a dataset, y should not be specified since targets will be obtained from x
-            validation_data = val_ds,   # data on which to evaluate the loss and any model metrics
+            validation_data = modelo.val_ds,   # data on which to evaluate the loss and any model metrics
                                         # the model will not be trained on this data
             epochs=3
             )
-    print(dir(model))
 
 if __name__ == "__main__":
     main()
